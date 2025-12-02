7f5bf26ac9c4e8b2e620e22994c8b2b9046982b6
muu-ima <enyukari.k.imamura@gmail.com>
2025-12-02 17:39:58 +0900

    再々修正

--- features/products/hooks/useProductForm.ts
+++ features/products/hooks/useProductForm.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { CategorySlug } from "@/features/products/constans";
+import type { CategorySlug } from "@/features/products/constants";
 import {
   type FormState,
   type ProductInitial,
